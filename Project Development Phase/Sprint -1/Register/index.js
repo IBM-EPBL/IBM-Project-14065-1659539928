f968ce71da01b7b30fe27d6019e83ff10ed3b417
SHIVA SANKARI C <54460360+shivasankari3101@users.noreply.github.com>
2022-10-30 19:13:10 +0530

    Add files via upload

---
+++ Project Development Phase/Sprint -1/Register/index.js
@@ -0,0 +1,27 @@
+function hide_password() {
+    document.getElementsByClassName("fa-eye")[0].classList.toggle("fa-eye-slash");
+    var x = document.getElementById("password");
+    if (x.type === "password") {
+        x.type = "text";
+    } else {
+        x.type = "password";
+    }
+}
+
+function hide_con_password() {
+    document.getElementsByClassName("fa-eye")[1].classList.toggle("fa-eye-slash");
+    var x = document.getElementById("con_password");
+    if (x.type === "password") {
+        x.type = "text";
+    } else {
+        x.type = "password";
+    }
+}
+
+function check_password(){
+    let y =  document.getElementById("password");
+    let y1 = document.getElementById("con_password");
+    if(y.value!=y1.value){
+        alert("Passwords mismatched");
+    }
+}
