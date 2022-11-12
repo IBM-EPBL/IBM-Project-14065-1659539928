83388f5e9b9ce8c13727ac87502c63523659b0af
Soundharya-64 <73594379+Soundharya-64@users.noreply.github.com>
2022-11-12 20:34:48 +0530

    Update index.js

--- Project Development Phase/Sprint -1/Register/index.js
+++ Project Development Phase/Sprint -1/Register/index.js
@@ -1,4 +1,5 @@
-function hide_password() {
+
+ function hide_password() {
     document.getElementsByClassName("fa-eye")[0].classList.toggle("fa-eye-slash");
     var x = document.getElementById("password");
     if (x.type === "password") {
@@ -18,10 +19,24 @@ function hide_con_password() {
     }
 }
 
+
 function check_password(){
     let y =  document.getElementById("password");
     let y1 = document.getElementById("con_password");
     if(y.value!=y1.value){
         alert("Passwords mismatched");
+        return false
     }
+    else{
+        return true
+    }
+}
+
+function validate_password(password){
+     if(password < 6){
+      return false;
+     }
+     else{
+        return true;
+     }
 }
