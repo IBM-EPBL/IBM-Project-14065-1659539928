46ab4b89bae65f218980799f190581f1becd2073
Soundharya-64 <73594379+Soundharya-64@users.noreply.github.com>
2022-11-12 20:33:26 +0530

    Add files via upload

---
+++ Project Development Phase/Sprint -1/Login/index.js
@@ -0,0 +1,10 @@
+
+function hide_password() {
+    document.getElementsByClassName("fa-eye")[0].classList.toggle("fa-eye-slash");
+    var x = document.getElementById("password");
+    if (x.type === "password") {
+        x.type = "text";
+    } else {
+        x.type = "password";
+    }
+}
